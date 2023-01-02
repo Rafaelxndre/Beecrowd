b316818bdd2df633ab03489793688d5554f27579
Rafael Alexandre <rafaelxndre@gmail.com>
2023-01-02 18:16:42 -0300

    NVL1 - 5/336 resolvidos

---
+++ PRB_NVL1/src/NVL1_1004.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NVL1_1004 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int A, B, PROD;
+
+        A = entrada.nextInt();
+        B = entrada.nextInt();
+
+        PROD = A * B;
+
+        System.out.println("PROD = "+PROD);
+
+    }
+}
