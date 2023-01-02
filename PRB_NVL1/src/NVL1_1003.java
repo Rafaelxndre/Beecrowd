64b6aa034736f7feefac5596484628859d53c1ea
Rafael Alexandre <rafaelxndre@gmail.com>
2023-01-02 18:02:03 -0300

    NVL1 - 4/336 resolvidos

---
+++ PRB_NVL1/src/NVL1_1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NVL1_1003 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int A, B, SOMA;
+
+        A = entrada.nextInt();
+        B = entrada.nextInt();
+
+        SOMA = A + B;
+
+        System.out.println("SOMA = "+SOMA);
+    }
+}
